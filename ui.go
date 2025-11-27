c03d66576b147f6dc1ecbb7611e8344cbb9e7ced
rrakea <login@konradburgi.com>
2025-11-27 13:24:04 +0100

    Added

--- ui.go
+++ ui.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -12,9 +14,13 @@ type Model struct {
 	packages table.Model
 	row_map  map[string]Package
 	info     string
+	help     help.Model
+	keys     Keymap
 }
 
 var input_chan chan string
+
+const SEARCH_BOX_HEIGHT = 10
 
 func default_model() Model {
 	input := textinput.New()
@@ -27,12 +33,14 @@ func default_model() Model {
 		{Title: "Name", Width: 40},
 	}
 	rows := []table.Row{}
-	table := table.New(table.WithColumns(columns), table.WithRows(rows), table.WithFocused(false), table.WithHeight(10))
+	table := table.New(table.WithColumns(columns), table.WithRows(rows), table.WithFocused(false), table.WithHeight(SEARCH_BOX_HEIGHT))
 
 	return Model{
 		input:    input,
 		packages: table,
 		info:     info(Package{}),
+		help:     help.New(),
+		keys:     Default_keymap(),
 	}
 }
 
@@ -51,24 +59,24 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "enter":
+		switch {
+		case key.Matches(msg, m.keys.keys["install"]):
 			row := m.packages.SelectedRow()
 			if len(row) > 0 {
 				to_install = m.row_map[row[0]].Name
 			}
 			cmd = tea.Quit
-		case "down":
+		case key.Matches(msg, m.keys.keys["down"]):
 			if len(m.packages.Rows()) != 0 {
 				m.packages.MoveDown(1)
 				m.info = info(m.row_map[m.packages.SelectedRow()[0]])
 			}
-		case "up":
+		case key.Matches(msg, m.keys.keys["up"]):
 			if len(m.packages.Rows()) != 0 {
 				m.packages.MoveUp(1)
 				m.info = info(m.row_map[m.packages.SelectedRow()[0]])
 			}
-		case "esc", "crtl+c":
+		case key.Matches(msg, m.keys.keys["quit"]):
 			cmd = tea.Quit
 		default:
 			input_chan <- m.input.Value()
@@ -83,7 +91,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 
 		m.packages.SetRows(rows)
-		m.info = info(m.row_map[m.packages.SelectedRow()[0]])
+		row := m.packages.SelectedRow()
+		if len(row) > 0 {
+			m.info = info(m.row_map[row[0]])
+		}
 	}
 
 	m.packages, cmd_table = m.packages.Update(msg)
@@ -96,21 +107,18 @@ func (m Model) View() string {
 		Border(lipgloss.RoundedBorder(), true).
 		BorderForeground(lipgloss.Color("#cba6f7"))
 
-	top := lipgloss.JoinHorizontal(0, style.Render(m.packages.View()), style.Width(40).Render(m.info))
-	full := lipgloss.JoinVertical(0, top, style.Width(84).Render(m.input.View()))
+	help := m.help.View(m.keys)
+	pack := style.Render(m.packages.View())
+	info := style.Width(40).Render(m.info)
+	input := style.Width(84).Render(m.input.View())
+
+	top := lipgloss.JoinHorizontal(0, pack, info)
+	full := lipgloss.JoinVertical(0, top, help, input)
 
 	return full
 }
 
 func info(p Package) string {
-	if p.Name == "" {
-		buf := ""
-		for range 9 {
-			buf += "\n"
-		}
-		return buf
-	}
-
 	var out_of_date string
 	if p.Out_of_date == 0 {
 		out_of_date = "No"
@@ -125,11 +133,11 @@ func info(p Package) string {
 		maintainer = p.Maintainer
 	}
 
-	return "Name: " + p.Name + "\n" +
+	str := "Name: " + p.Name + "\n" +
 		"Version: " + p.Version + "\n" +
 		"URL: " + p.Url + "\n" +
 		"Out of Date: " + out_of_date + "\n" +
 		"Maintainer: " + maintainer + "\n" +
 		"Description: " + p.Desc
-
+	return str
 }
